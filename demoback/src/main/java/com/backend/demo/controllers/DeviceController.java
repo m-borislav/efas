db9ad3ccba34eb1fa92420482c6f77b07a17c031
Borislav <marusovborislav@gmail.com>
2020-07-29 12:49:16 +0300

    Back and front

--- demoback/src/main/java/com/backend/demo/controllers/DeviceController.java
+++ demoback/src/main/java/com/backend/demo/controllers/DeviceController.java
@@ -2,7 +2,6 @@ package com.backend.demo.controllers;
 
 
 import com.backend.demo.models.Device;
-import com.backend.demo.models.Equipment;
 import com.backend.demo.repos.DeviceRepository;
 import com.backend.demo.repos.EquipmentRepository;
 import com.backend.demo.services.DeviceService;
@@ -15,12 +14,13 @@ import java.util.List;
 
 @RestController
 public class DeviceController {
+
+    @Autowired
     private DeviceRepository deviceRepository;
     private DeviceService deviceService;
     private EquipmentRepository equipmentRepository;
 
 
-    @Autowired
     public DeviceController(DeviceRepository deviceRepository, DeviceService deviceService, EquipmentRepository equipmentRepository) {
         this.deviceRepository = deviceRepository;
         this.deviceService = deviceService;
@@ -34,15 +34,15 @@ public class DeviceController {
     }
 
     @GetMapping(value = "/device")
-    public List<Device> findAllDevices(){
+    public List<Device> findAllDevices() {
         return (List<Device>) deviceRepository.findAll();
     }
 
     // TO DO: test after adding equipment
 
     @GetMapping(value = "/equipment{equipmentId}/device")
-    public List<Device> findDevicesByEquipment(@PathVariable Long equipmentId){
-      return (List<Device>) deviceRepository.findByEquipmentId(equipmentId);
+    public List<Device> findDevicesByEquipment(@PathVariable Long equipmentId) {
+        return (List<Device>) deviceRepository.findByEquipmentId(equipmentId);
     }
 
     @DeleteMapping(value = "/device/{id}")
